38af3ac888ad3f463e05e6eb97b32abb8cf9193e
Andrade Sig <tssignorini@gmail.com>
2021-06-06 15:49:25 -0300

    Adição da página single do produto

--- src/app/pages/produto-detalhe/produto-detalhe.component.ts
+++ src/app/pages/produto-detalhe/produto-detalhe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+//declare function hello(): void;
 
 @Component({
   selector: 'app-produto-detalhe',
@@ -7,7 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoDetalheComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+    $(function(){
+
+      var imagens = $('.product-image').length;
+      var imagemAtual = 0;
+      
+      atualizarContagemImagens();
+      avancarImagem();
+      voltarImagem();
+
+      $('.product-image').eq(imagemAtual).css('display','block');
+
+      function avancarImagem(){
+          $('#botao-prosseguir').click(function(){
+            $('.product-image').eq(imagemAtual).css('display','none');
+            imagemAtual++;
+            if(imagemAtual > imagens - 1){
+                imagemAtual = imagens - 1;
+            }
+            $('.product-image').eq(imagemAtual).css('display','block');
+          });
+      }
+
+      function voltarImagem(){
+          $('#botao-voltar').click(function(){
+            $('.product-image').eq(imagemAtual).css('display','none');
+            imagemAtual--;
+            if(imagemAtual < 0){
+                imagemAtual = 0;
+            }
+            $('.product-image').eq(imagemAtual).css('display','block');
+          });
+      }
+
+      function atualizarContagemImagens(){
+        setInterval(function(){
+          var numeroTotalImagens = $('.product-image').length;
+          var numeroAtualImagem = imagemAtual + 1;
+          var textoContagem = document.getElementById('numero-imagens');
+          textoContagem.innerHTML = "(" + numeroAtualImagem + "/" + numeroTotalImagens + ")";
+        });
+      }
+
+    });
+
+  }
 
   ngOnInit() {
   }
