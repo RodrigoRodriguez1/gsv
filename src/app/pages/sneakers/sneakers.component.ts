060d1d87ff2f47e244a75e2d1a70d1fd90b1890e
RodrigoRodriguez1 <rodrigorfig1@gmail.com>
2021-10-12 19:53:50 -0300

    filtro completo de az, za, -+, +-

--- src/app/pages/sneakers/sneakers.component.ts
+++ src/app/pages/sneakers/sneakers.component.ts
@@ -76,40 +76,28 @@ export class SneakersComponent implements OnInit {
 
   filtros(aux) {
 
-    var a = {
-      nome: 'd',
-      preco: 23
-    }
-    var b = {
-      nome: 'z',
-      preco: 11
-
-    }
-    var c = {
-      nome: 'g',
-      preco: 13
-
-    }
-
-    var array = [];
-
-    array = [a, b, c];
-    if (aux == 'az') {
-      array.sort(function (a, b) {
+    if (aux.value == 'az') {
+     this.produtos.sort(function (a, b) {
         if (a.nome < b.nome) { return -1; }
         if (a.nome > b.nome) { return 1; }
         return 0;
       })
     }
-    if (aux == 'za')
-      array.sort(function (a, b) {
+    if (aux.value  == 'za')
+    this.produtos.sort(function (a, b) {
         if (a.nome > b.nome) { return -1; }
         if (a.nome < b.nome) { return 1; }
         return 0;
       })
 
-      array.sort((a, b) => a.preco - b.preco)
-      array.sort((a, b) => b.preco - a.preco)
+      if(aux.value == "pmaiormenor"){
+        this.produtos.sort((a, b) => b.preco - a.preco)
+
+      }
+      if(aux.value == "pmenormaior") {
+        this.produtos.sort((a, b) => a.preco - b.preco)
+
+      }
       debugger
 
 
