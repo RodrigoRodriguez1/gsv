ea5fad77e2c0d8b85d59b8477eb6721d45331686
LCSRuas <lucasiruas@gmail.com>
2021-12-02 19:34:02 -0300

    ajustes pedidos pelo cliente

--- src/app/pages/produto-detalhe/produto-detalhe.component.ts
+++ src/app/pages/produto-detalhe/produto-detalhe.component.ts
@@ -96,9 +96,9 @@ export class ProdutoDetalheComponent implements OnInit {
 
   customOptions: OwlOptions = {
     loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
     dots: false,
     navSpeed: 700,
     navText: ['', ''],
